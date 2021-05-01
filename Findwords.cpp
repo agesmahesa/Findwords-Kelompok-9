38213be3d9183de4b74a05e49ad59ad01a6bd5c2
Ages Mahesa <78712553+agesmahesa@users.noreply.github.com>
2021-05-01 22:31:54 +0700

    Update Findwords.cpp

--- Findwords.cpp
+++ Findwords.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+const int cols = 16, rows = 15;
 
+char words[rows][cols] = {"tgbwwinterwsesn",
+                                "aaunttmmhfoodnb",
+                                "jlwcqldzmpmvdmr",
+                                "asagmquwvvbsohi",
+                                "bwplotanadtpgnc",
+                                "rewngodjcpnatnk",
+                                "eeotwosbqharrsa",
+                                "azcgeswewnaknpb",
+                                "dinnerqodlwdcar",
+                                "onopkwmparktzcc",
+                                "qbfrogmamwpweey",
+                                "lqzqnnmrzjjsclg",
+                                "mosgzczetdbooto",
+                                "pdcrzmsngrdnrpz",
+                                "ohnkzwaterjgtra"};
