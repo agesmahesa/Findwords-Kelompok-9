fde8a67c4d556da809fcb5a0f7618b032deb13c6
Ages Mahesa <78712553+agesmahesa@users.noreply.github.com>
2021-05-02 23:06:35 +0700

    Update Findwords.cpp
    program utama

--- Findwords.cpp
+++ Findwords.cpp
@@ -57,7 +57,6 @@ int cari_kata(char *(masukkan)){
 					kata_ketemu+=0;
 				}
 				cek=0;
-
 				//cari kata vertikal bawah
 				for (int n=0; n<panjang; n++){
 					if (masukkan[n]==words[i+n][j]){
@@ -90,4 +89,27 @@ int cari_kata(char *(masukkan)){
 			
 		}
 	}
+		
+	if(kata_ketemu>0){
+		cout<<"Ada"<<endl;
+	}else{
+		cout<<"Tidak Ada"<<endl;
+	}
+	return 0;
+}
+
+int main(){
+	int x;
+	cin >> x;
+	
+	char masukkan[x][15];
+	for (int i=0; i<x; i++){
+		cin >> masukkan[i];
+	}
+	for (int i=0; i<x; i++){
+		cari_kata(*(masukkan+i));
+		
+	}	
+}
+
 	
