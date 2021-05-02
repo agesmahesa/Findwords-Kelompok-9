2289a10c8d54b65b7bacc32bad7abf13713bf01b
Ages Mahesa <78712553+agesmahesa@users.noreply.github.com>
2021-05-02 20:22:15 +0700

    Update Findwords.cpp
    fungsi untuk mencari kata

--- Findwords.cpp
+++ Findwords.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <cstring>
-using namespace std;
-const int cols = 16, rows = 15;
 
-char words[rows][cols] = {"tgbwwinterwsesn",
-                                "aaunttmmhfoodnb",
-                                "jlwcqldzmpmvdmr",
-                                "asagmquwvvbsohi",
-                                "bwplotanadtpgnc",
-                                "rewngodjcpnatnk",
-                                "eeotwosbqharrsa",
-                                "azcgeswewnaknpb",
-                                "dinnerqodlwdcar",
-                                "onopkwmparktzcc",
-                                "qbfrogmamwpweey",
-                                "lqzqnnmrzjjsclg",
-                                "mosgzczetdbooto",
-                                "pdcrzmsngrdnrpz",
-                                "ohnkzwaterjgtra"};
+using namespace std;
+const int kolom = 16, baris = 15;
+
+int cari_kata(char *(masukkan)){
+	int panjang, kata_ditemukan, cek;
+	kata_ditemukan=0;
+	panjang=strlen(masukkan);
+	char words[baris][kolom] = {"tgbwwinterwsesn",
+                              "aaunttmmhfoodnb",
+                            	"jlwcqldzmpmvdmr",
+                           		"asagmquwvvbsohi",
+                              "bwplotanadtpgnc",
+                              "rewngodjcpnatnk",
+                              "eeotwosbqharrsa",
+                              "azcgeswewnaknpb",
+                              "dinnerqodlwdcar",
+                              "onopkwmparktzcc",
+	                            "qbfrogmamwpweey",
+								              "lqzqnnmrzjjsclg",
+							              	"mosgzczetdbooto",
+								              "pdcrzmsngrdnrpz",
+								              "ohnkzwaterjgtra"};
