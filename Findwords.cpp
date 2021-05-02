31e7b41b011a1e067459b8c0f231baf69f8a26b3
Ages Mahesa <78712553+agesmahesa@users.noreply.github.com>
2021-05-02 22:36:51 +0700

    Update Findwords.cpp
    cari kata horizontal

--- Findwords.cpp
+++ Findwords.cpp
@@ -2,32 +2,62 @@
 #include <cstring>
 
 using namespace std;
+
 const int kolom = 16, baris = 15;
 
+char words[baris][kolom] = {"tgbwwinterwsesn",
+	                    "aaunttmmhfoodnb",
+                            "jlwcqldzmpmvdmr",
+                            "asagmquwvvbsohi",
+			    "bwplotanadtpgnc",
+			    "rewngodjcpnatnk",
+			    "eeotwosbqharrsa",
+			    "azcgeswewnaknpb",
+			    "dinnerqodlwdcar",
+			    "onopkwmparktzcc",
+			    "qbfrogmamwpweey",
+			    "lqzqnnmrzjjsclg",
+			    "mosgzczetdbooto",
+			    "pdcrzmsngrdnrpz",
+			    "ohnkzwaterjgtra"};
+			
 int cari_kata(char *(masukkan)){
-	int panjang, kata_ditemukan, cek;
-	kata_ditemukan=0;
+	int panjang, kata_ketemu, cek;
+	kata_ketemu=0;
 	panjang=strlen(masukkan);
-	char words[baris][kolom] = {"tgbwwinterwsesn",
-                              "aaunttmmhfoodnb",
-                            	"jlwcqldzmpmvdmr",
-                              "asagmquwvvbsohi",
-				                      "bwplotanadtpgnc",
-				                      "rewngodjcpnatnk",
-			                       	"eeotwosbqharrsa",
-			                      	"azcgeswewnaknpb",
-		                      		"dinnerqodlwdcar",
-			                      	"onopkwmparktzcc",
-			                       	"qbfrogmamwpweey",
-			                      	"lqzqnnmrzjjsclg",
-			                      	"mosgzczetdbooto",
-			                      	"pdcrzmsngrdnrpz",
-			                      	"ohnkzwaterjgtra"};
+	
+    for (int i=0; i<15; i++){
+		for (int j=0; j<15 ;j++){
+			if (masukkan[0]==words[i][j]){
+				//cari kata horizontal kanan
+				for (int n=0; n<panjang; n++){
+					if (masukkan[n]==words[i][j+n]){
+						cek=n;
+				}else{
+					break;
+				}
+				}
+				if (cek==panjang-1){
+					kata_ketemu+=1;
+				}else{
+					kata_ketemu+=0;
+				}
+				cek=0;
+				//cari kata horizontal kiri
+				for (int n=0; n<panjang; n++){
+					if (masukkan[n]==words[i][j-n]){
+						cek=n;
+					}else{
+						break;
+					}
+				}
+				if (cek==panjang-1){
+					kata_ketemu+=1;
+				}else{
+					kata_ketemu+=0;
+				}
+				cek=0;
 
-char *getWordVertical(int);
-char *reverse(char *);
-bool searchVertical(char *);
-bool searchHorizontal(char *);
 
 
 
