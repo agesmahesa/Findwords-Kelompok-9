a389a5e835bb46c6f6526fb45a956957e40f92a3
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2021-05-02 20:43:07 +0700

    perbaikkan declared dan Int main

--- Findwords.cpp
+++ Findwords.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
 #include <cstring>
-using namespace std;
-const int cols = 16, rows = 15;
 
-char words[rows][cols] = {"tgbwwinterwsesn",
-                                "aaunttmmhfoodnb",
-                                "jlwcqldzmpmvdmr",
-                                "asagmquwvvbsohi",
-                                "bwplotanadtpgnc",
-                                "rewngodjcpnatnk",
-                                "eeotwosbqharrsa",
-                                "azcgeswewnaknpb",
-                                "dinnerqodlwdcar",
-                                "onopkwmparktzcc",
-                                "qbfrogmamwpweey",
-                                "lqzqnnmrzjjsclg",
-                                "mosgzczetdbooto",
-                                "pdcrzmsngrdnrpz",
-                                "ohnkzwaterjgtra"};
+using namespace std;
+const int kolom = 16, baris = 15;
+
+char words[baris][kolom] = {"tgbwwinterwsesn",
+                        	"aaunttmmhfoodnb",
+                            "jlwcqldzmpmvdmr",
+                            "asagmquwvvbsohi",
+                            "bwplotanadtpgnc",
+                            "rewngodjcpnatnk",
+                            "eeotwosbqharrsa",
+                            "azcgeswewnaknpb",
+                            "dinnerqodlwdcar",
+                            "onopkwmparktzcc",
+                            "qbfrogmamwpweey",
+                            "lqzqnnmrzjjsclg",
+                            "mosgzczetdbooto",
+                            "pdcrzmsngrdnrpz",
+                            "ohnkzwaterjgtra"};
+
 char *getWordVertical(int);
 char *reverse(char *);
 bool searchVertical(char *);
 bool searchHorizontal(char *);
 
 
+
+
 int main(){
-  return 0;
-  
+	system("color 64");
+    char word[16];
+    int n;
+    cin>>n;
+    cin.ignore();
+    
+    for (int x=0; x<n; x++){
+        cin.getline(word, 16,'\n');
+        
+        if (CARIHorizontal(word)){
+            cout << "Ada\n";
+        }else if (CARIVertical(word)){
+            cout << "Ada\n";
+        }else{
+           cout << "Tidak Ada\n";
+        }
+    }
+    return 0;
 }
