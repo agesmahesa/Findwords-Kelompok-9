3508cb2b83945befbdbe89dd62572a1eb4dce118
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2021-05-10 15:24:39 +0700

    menambahkan fungsi utama

--- Findwords.cpp
+++ Findwords.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 
 using namespace std;
+
 const int kolom = 16, baris = 15;
 
 char words[baris][kolom] = {"tgbwwinterwsesn",
@@ -25,23 +26,52 @@ char *reverse(char *);
 bool searchVertical(char *);
 bool searchHorizontal(char *);
 
+char *getWordVertical(int dapat){
+	char *cekVertikal=(char*) malloc(16*sizeof(char));
+	for(int i=0; i<baris; i++)
+		cekVertikal[i] = words[i][dapat];
+	return cekVertikal;
+}
+
+char *reverse(char *masukkan){
+	char *cek1, *cek2;
+	if(! masukkan || ! *masukkan)	
+	for (cek1=masukkan, cek2=masukkan+strlen(masukkan)-1; cek2>cek1; ++cek1, --cek2){
+        *cek1 ^= *cek2;
+        *cek2 ^= *cek1;
+        *cek1 ^= *cek2;
+	}
+	return masukkan;
+}
+
+//cari kata horizontal
+bool searchHorizontal(char *cari_kata){
+	for(int i=0; i<=15; i++)
+		if(strstr(words[i], cari_kata) != NULL)
+			return true;
+			
+	for(int i=0; i<=15; i++){
+		if(strstr(reverse(words[i]), cari_kata) != NULL)
+			return true;
+	}
+	return false;
+}
+
+//fungsi utama
 int main(){
-    system("color 64");
+	
     char word[16];
-    int n;
-    cin>>n;
-    cin.ignore();
-    
-    for (int x=0; x<n; x++){
+    int x;
+    cin >> x;
+	cin.ignore();
+	
+	for (int i=0; i<x; i++){
         cin.getline(word, 16,'\n');
         
-        if (CARIHorizontal(word)){
+        if (searchVertical(word) || searchHorizontal(word))
             cout << "Ada\n";
-        }else if (CARIVertical(word)){
-            cout << "Ada\n";
-        }else{
-           cout << "Tidak Ada\n";
-        }
+        else 
+            cout << "Tidak Ada\n";
     }
     return 0;
 }
